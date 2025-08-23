d6b6159031bd98e8e401e2b19cdb315e784a220f
Ревотюк Даниил <danistollen@yahoo.com>
2025-08-23 18:55:05 +0300

    DI for password hasher

--- SolarLab.EBoard.Infrastructure/DependencyInjection.cs
+++ SolarLab.EBoard.Infrastructure/DependencyInjection.cs
@@ -1,7 +1,9 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using SolarLab.EBoard.Application.Abstractions.Authentication;
 using SolarLab.EBoard.Domain.Interfaces;
+using SolarLab.EBoard.Infrastructure.Authentication;
 using SolarLab.EBoard.Infrastructure.Persistence;
 
 namespace SolarLab.EBoard.Infrastructure;
@@ -12,7 +14,8 @@ public static class DependencyInjection
     {
         return services
             .AddServices()
-            .AddDatabase(configuration);
+            .AddDatabase(configuration)
+            .AddAuthenticationInternal(configuration);
     }
 
     private static IServiceCollection AddServices(this IServiceCollection services)
@@ -29,6 +32,13 @@ public static class DependencyInjection
         services.AddScoped<IAdPostsRepository, AdPostsRepository>();
         services.AddScoped<IUsersRepository, UsersRepository>();
         
+        return services;
+    }
+
+    private static IServiceCollection AddAuthenticationInternal(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddSingleton<IPasswordHasher, PasswordHasher>();
+
         return services;
     }
 }
